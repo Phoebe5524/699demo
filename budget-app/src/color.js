381c4fb5592a0723252564b6ab4b9828791cb66e
Phoebe5524 <lyuechen@umich.edu>
2025-11-24 13:11:54 -0500

    add two files

---
+++ budget-app/src/color.js
@@ -0,0 +1,12 @@
+const CATEGORIES_CONFIG = {
+    'Food': { icon: <Utensils size={14} />, color: 'bg-[#f6cccc]', label: 'Food' }, 
+    'Transportation': { icon: <Car size={14} />, color: 'bg-[#fffdc0]', label: 'Trans' }, 
+    'Shopping': { icon: <ShoppingCart size={14} />, color: 'bg-[#d8ccdd]', label: 'Shop' }, 
+    'Entertainment': { icon: <Clapperboard size={14} />, color: 'bg-[#f7c7a6]', label: 'Fun' }, 
+    'Personal Care': { icon: <Sparkles size={14} />, color: 'bg-[#d8f7fd]', label: 'Care' }, 
+    'Education': { icon: <GraduationCap size={14} />, color: 'bg-[#bba0c6]', label: 'Edu' }, 
+    'Savings': { icon: <PiggyBank size={14} />, color: 'bg-[#dbe3cb]', label: 'Save' }, 
+    'Subscription': { icon: <Mail size={14} />, color: 'bg-[#bbecf3]', label: 'Sub' }, 
+    'Misc': { icon: <Zap size={14} />, color: 'bg-[#babab9]', label: 'Misc' }, 
+    'Income': { icon: <Plus size={14} />, color: 'bg-gray-800', label: 'Inc' }, 
+  };
